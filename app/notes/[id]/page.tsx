46e02ace01a9cc7c250c9cb635b97ecf38554d28
S.Yevhenii.Serhiiovych <s.yevhenii.shevchenko@gmail.com>
2026-04-23 19:03:13 +0300

    implement metadata and fonts

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -3,6 +3,7 @@ import {
   HydrationBoundary,
   QueryClient,
 } from "@tanstack/react-query";
+import type { Metadata } from "next";
 
 import noteService from "@/lib/api";
 
@@ -10,6 +11,32 @@ import NoteDetailsClient from "./NoteDetails.client";
 
 interface NoteDetailsPageProps {
   params: Promise<{ id: string }>;
+}
+
+export async function generateMetadata({
+  params,
+}: NoteDetailsPageProps): Promise<Metadata> {
+  const { id } = await params;
+  const note = await noteService.fetchNoteById(id);
+
+  return {
+    title: `${note.title} | NoteHub`,
+    description: note.content?.slice(0, 150) || "Note details page",
+    openGraph: {
+      title: note.title,
+      description: note.content?.slice(0, 150) || "Note details page",
+      url: `https://08-zustand-red-tau.vercel.app/notes/${note.id}`,
+      siteName: "NoteHub",
+      images: [
+        {
+          url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+          width: 1200,
+          height: 630,
+          alt: "NoteHub Open Graph Image",
+        },
+      ],
+    },
+  };
 }
 
 export default async function NoteDetailsPage({
